691b48769a87473af91d69bbd7606740d743befe
Samuel Obeng <61053627+samlobeng@users.noreply.github.com>
2021-06-16 09:12:32 +0200

    Education fixes

--- src/components/Education/Description.jsx
+++
@@ -1,9 +0,0 @@
-export default function Description({ courses }) {
-  return (
-    <>
-      <div className="education-description px-5">
-        <h6 className="description">{courses}</h6>
-      </div>
-    </>
-  )
-}
