aabafcb0a18a1b78633dc22c8df16a382ebb38d6
Luna Sarmiento <selene942008@hotmail.com>
2021-09-08 21:41:08 +0200

    Post try

--- src/components/PostModal/PostModal.jsx
+++ src/components/PostModal/PostModal.jsx
@@ -1,35 +1,35 @@
-import { Modal, Button, Form } from "react-bootstrap"
-import { getProfileById, addPost } from "../assets/fetch"
+import { Modal, Button, Form } from "react-bootstrap";
+import { getProfileById, addPost } from "../assets/fetch";
 
-import { useEffect, useState } from "react"
-import "./PostModal.css"
-import UploadImage from "../assets/UploadImage"
+import { useEffect, useState } from "react";
+import "./PostModal.css";
+import UploadImage from "../assets/UploadImage";
 
 const PostModal = ({ show, onHide, onUpdate }) => {
-  const [myProfile, setMyProfile] = useState({})
-  const [pictureFile, setPictureFile] = useState(null)
+  const [myProfile, setMyProfile] = useState({});
+  const [pictureFile, setPictureFile] = useState(null);
 
   const [data, setData] = useState({
     text: "",
     image: "",
-  })
+  });
 
   useEffect(() => {
-    getProfileById("me", setMyProfile)
-  }, [])
+    getProfileById("me", setMyProfile);
+  }, []);
 
-  const getData = e => {
-    setData({ text: e.currentTarget.value })
-  }
+  const getData = (e) => {
+    setData({ text: e.currentTarget.value });
+  };
 
   const handlePost = () => {
-    let formData = null
+    let formData = null;
     if (pictureFile) {
-      formData = new FormData()
-      formData.append("post", pictureFile)
+      formData = new FormData();
+      formData.append("post", pictureFile);
     }
-    addPost(data, formData)
-  }
+    addPost(data, formData);
+  };
 
   return (
     <Modal show={show} onHide={onHide}>
@@ -43,29 +43,37 @@ const PostModal = ({ show, onHide, onUpdate }) => {
         <Form
           id="post-form"
           className="w-100"
-          onSubmit={e => {
-            e.preventDefault()
-            handlePost()
-            setData({ text: "" })
+          onSubmit={(e) => {
+            e.preventDefault();
+            handlePost();
+            setData({ text: "" });
             setTimeout(() => {
-              onUpdate()
-            }, 2000)
-            onHide()
+              onUpdate();
+            }, 2000);
+            onHide();
           }}
         >
-          <Form.Group className="mb-3 post-text" controlId="exampleForm.ControlTextarea1">
+          <Form.Group
+            className="mb-3 post-text"
+            controlId="exampleForm.ControlTextarea1"
+          >
             <Form.Control
               as="textarea"
               rows={3}
               placeholder="What do you want to talk about"
               value={data.text}
-              onChange={e => getData(e)}
+              onChange={(e) => getData(e)}
               required
             />
           </Form.Group>
           <Form.Group>
             <UploadImage />
-            <Form.Control className="d-none" id="file-input" type="file" onChange={e => setPictureFile(e.target.files[0])} />
+            <Form.Control
+              className="d-none"
+              id="file-input"
+              type="file"
+              onChange={(e) => setPictureFile(e.target.files[0])}
+            />
           </Form.Group>
         </Form>
       </Modal.Body>
@@ -75,6 +83,6 @@ const PostModal = ({ show, onHide, onUpdate }) => {
         </Button>
       </Modal.Footer>
     </Modal>
-  )
-}
-export default PostModal
+  );
+};
+export default PostModal;
--- src/components/ProfilePage/ProfilePage.jsx
+++ src/components/ProfilePage/ProfilePage.jsx
@@ -1,42 +1,53 @@
-import { Row, Col } from "react-bootstrap"
-import ActivitySection from "../ActivitySection/ActivitySection"
-import AlsoViewed from "../AlsoViewedSection/AlsoViewedSection"
-import HeroSection from "../HeroSection/HeroSection"
+import { Row, Col } from "react-bootstrap";
+import ActivitySection from "../ActivitySection/ActivitySection";
+import AlsoViewed from "../AlsoViewedSection/AlsoViewedSection";
+import HeroSection from "../HeroSection/HeroSection";
 // import DashboardSection from "../DashboardSection/DashboardSection"
-import AboutSection from "../AboutSection/AboutSection"
-import FeaturedRow from "../Featured/FeaturedRow"
-import EducationRow from "../Education/EducationRow"
-import ExperienceRow from "../Experience/ExperienceRow"
-import { useState, useEffect } from "react"
-import { getExperiencesById, getPosts, getProfileById } from "../assets/fetch"
+import AboutSection from "../AboutSection/AboutSection";
+import FeaturedRow from "../Featured/FeaturedRow";
+import EducationRow from "../Education/EducationRow";
+import ExperienceRow from "../Experience/ExperienceRow";
+import { useState, useEffect } from "react";
+import { getExperiencesById, getPosts, getProfileById } from "../assets/fetch";
 
-const ProfilePage = props => {
-  const currentUserId = props.match.params.userId
-  const isMe = currentUserId === "me" // boolean
-  const [profileData, setProfileData] = useState({})
-  const [experiences, setExperiences] = useState([])
-  const [posts, setPosts] = useState(null)
-  const [isRefreshed, setIsRefreshed] = useState(false)
+const ProfilePage = (props) => {
+  const currentUserId = props.match.params.userId;
+  const isMe = currentUserId === "me"; // boolean
+  const [profileData, setProfileData] = useState({});
+  const [experiences, setExperiences] = useState([]);
+  const [posts, setPosts] = useState(null);
+  const [isRefreshed, setIsRefreshed] = useState(false);
 
   useEffect(() => {
-    getProfileById(currentUserId, setProfileData)
-    getExperiencesById(currentUserId, setExperiences)
-    getPosts(setPosts)
-    setIsRefreshed(false)
-  }, [isRefreshed, currentUserId])
+    getProfileById(currentUserId, setProfileData);
+    getExperiencesById(currentUserId, setExperiences);
+    getPosts(setPosts);
+    setIsRefreshed(false);
+    console.log("experiences ->", experiences);
+  }, [isRefreshed, currentUserId]);
 
   const refresh = () => {
-    setIsRefreshed(true)
-  }
+    setIsRefreshed(true);
+  };
 
   return (
     <Row className="align-items-start">
       <Col className="mb-3 pb-1 mt-2" xs={12} md={9}>
         <div className="section-card">
-          <HeroSection profileData={profileData} experiences={experiences} userId={currentUserId} isMe={isMe} onUpdate={refresh} />
+          <HeroSection
+            profileData={profileData}
+            experiences={experiences}
+            userId={currentUserId}
+            isMe={isMe}
+            onUpdate={refresh}
+          />
         </div>
         <div className="section-card p-3">
-          <AboutSection profileData={profileData} isMe={isMe} onUpdate={refresh} />
+          <AboutSection
+            profileData={profileData}
+            isMe={isMe}
+            onUpdate={refresh}
+          />
         </div>
         <div className="section-card p-3">
           <FeaturedRow isMe={isMe} />
@@ -45,7 +56,11 @@ const ProfilePage = props => {
           <ActivitySection userPosts={posts} currentUserId={currentUserId} />
         </div>
         <div className="section-card p-3">
-          <ExperienceRow experiencesData={experiences} onUpdate={refresh} isMe={isMe} />
+          <ExperienceRow
+            experiencesData={experiences}
+            onUpdate={refresh}
+            isMe={isMe}
+          />
         </div>
         <div className="section-card p-3">
           <EducationRow isMe={isMe} />
@@ -57,7 +72,7 @@ const ProfilePage = props => {
         </div>
       </Col>
     </Row>
-  )
-}
+  );
+};
 
-export default ProfilePage
+export default ProfilePage;
