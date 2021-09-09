53dd3235e9a8869b110e410071483005c6e6567f
Luna Sarmiento <selene942008@hotmail.com>
2021-09-09 15:14:15 +0200

    Update ActivitySection.jsx

--- src/components/ActivitySection/ActivitySection.jsx
+++ src/components/ActivitySection/ActivitySection.jsx
@@ -1,20 +1,25 @@
-import ActivityItem from "./ActivityItem"
+import ActivityItem from "./ActivityItem";
+import { MY_ID } from "../assets/fetch";
 
-import "./ActivitySection.css"
+import "./ActivitySection.css";
 
 const ActivitySection = ({ userPosts, currentUserId }) => {
   const filterArray = () => {
-    const userId = currentUserId === "me" ? "60c70adc291930001560ab93" : currentUserId
-    const filteredArr = userPosts?.filter(post => post?.user?._id === userId)
-    return filteredArr.map(post => <ActivityItem key={post._id} post={post} />).slice(0,6)
-  }
+    const userId = currentUserId === "me" ? MY_ID : currentUserId;
+    const filteredArr = userPosts?.filter((post) => post?.user?._id === userId);
+    return filteredArr
+      .map((post) => <ActivityItem key={post._id} post={post} />)
+      .slice(0, 6);
+  };
 
   return (
     <section>
       <h5>Activity</h5>
-      {userPosts && <div className="activity-section d-flex flex-wrap">{filterArray()}</div>}
+      {userPosts && (
+        <div className="activity-section d-flex flex-wrap">{filterArray()}</div>
+      )}
     </section>
-  )
-}
+  );
+};
 
-export default ActivitySection
+export default ActivitySection;
